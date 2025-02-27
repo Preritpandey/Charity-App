c8d0c3882bf039f259f95088090ba91696a1cf0d
PreritPandey <pandeyprerit45@gmail.com>
2025-02-27 21:36:13 +0545

    feat:added the deteails page

--- lib/pages/Home/event_details_page.dart
+++ lib/pages/Home/event_details_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:voln/resources/constant.dart';
 import 'package:voln/resources/text_heading.dart';
+import 'package:voln/resources/text_subheading.dart';
 import 'package:voln/widgets/app_text.dart';
 
 class DetailsPage extends StatelessWidget {
@@ -15,81 +16,82 @@ class DetailsPage extends StatelessWidget {
       body: Stack(
         children: [
           Positioned(
-              top: 0,
-              child: Container(
-                height: screenHeight * 0.25,
-                width: screenWidth,
-                color: Colors.red,
-                child: Text("data"),
-              )),
+            top: 0,
+            child: Container(
+              height: screenHeight * 0.3,
+              width: screenWidth,
+              color: Colors.red,
+              child: SizedBox(
+                height: screenHeight * 0.3,
+                width: double.infinity,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      child: Image.asset(
+                        'assets/children.png',
+                        width: screenWidth,
+                        height: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Positioned(
+                      left: screenWidth * 0,
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.arrow_back_ios_new)),
+                    ),
+                    Positioned(
+                      right: screenWidth * 0,
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.share),
+                      ),
+                    ),
+                    Positioned(
+                        top: screenHeight * 0.08,
+                        child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.grey.withOpacity(0.6),
+                                borderRadius: BorderRadius.circular(5)),
+                            child: const TextHeading(
+                              text: 'Details',
+                              color: Colors.black,
+                            )))
+                  ],
+                ),
+              ),
+            ),
+          ),
           Positioned(
-            height: ,
+              // height: ,
+              top: screenHeight * 0.3,
               child: Container(
-            height: screenHeight * 0.7,
-            width: screenWidth,
-            // color: Colors.black,
-            child: Text('data'),
-          ))
+                  height: screenHeight * 0.7,
+                  width: screenWidth,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(screenHeight * 0.03),
+                      topRight: Radius.circular(screenHeight * 0.03),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextHeading(text: 'Help Abandoned animals'),
+                      TextSubHeading(
+                          text:
+                              'Help abandoned animals in the shelter with us Animal Kaiser '),
+
+                              Row(children: [
+                                Container(),
+                                
+                              ],)
+                    ],
+                  )
+                  
+                  )),
         ],
       ),
-      // body: Column(
-      //   children: [
-      //     SizedBox(
-      //       height: screenHeight * 0.3,
-      //       width: double.infinity,
-      //       child: Stack(
-      //         children: [
-      //           Positioned(
-      //             child: Image.asset(
-      //               'assets/children.png',
-      //               width: screenWidth,
-      //               height: double.infinity,
-      //               fit: BoxFit.cover,
-      //             ),
-      //           ),
-      //           Positioned(
-      //             left: screenWidth * 0,
-      //             child: IconButton(
-      //                 onPressed: () {},
-      //                 icon: const Icon(Icons.arrow_back_ios_new)),
-      //           ),
-      //           Positioned(
-      //             right: screenWidth * 0,
-      //             child: IconButton(
-      //               onPressed: () {},
-      //               icon: const Icon(Icons.share),
-      //             ),
-      //           ),
-      //           Positioned(
-      //               top: screenHeight * 0.08,
-      //               child: Container(
-      //                   decoration: BoxDecoration(
-      //                       color: Colors.grey.withOpacity(0.6),
-      //                       borderRadius: BorderRadius.circular(5)),
-      //                   child: const TextHeading(
-      //                     text: 'Details',
-      //                     color: Colors.black,
-      //                   )))
-      //         ],
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: screenHeight * 0.8,
-      //       width: screenWidth,
-      //       child: Container(
-      //         decoration: BoxDecoration(
-      //           color: Colors.red,
-      //           borderRadius: BorderRadius.only(
-      //               topLeft: Radius.circular(10),
-      //               topRight: Radius.circular(10)),
-      //         ),
-      //         child: Column(
-      //           children: [Text('suywgs')],
-      //         ),
-      //       ),
-      //     ) // change later to 0.6 , as this screen will not have nav bar
-      //   ],
-      // ),
     );
   }
 }
---
+++ lib/widgets/details_status_icons.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DetailsStatus extends StatelessWidget {
+  const DetailsStatus({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [ImageIcon(AssetImage('assets/QuickBarIcons/medical.png'))],
+    );
+  }
+}
